12e9e970768e2271bc03efdb2df4019e64f181e6
Lars Snipen <lars.snipen@nmbu.no>
2020-07-15 12:30:03 +0000

    version 2.1

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -1,13 +1,13 @@
 ### R code from vignette source 'vignette.Rnw'
 
 ###################################################
-### code chunk number 1: vignette.Rnw:30-31 (eval = FALSE)
+### code chunk number 1: vignette.Rnw:33-34 (eval = FALSE)
 ###################################################
 ## system("blastp -h")
 
 
 ###################################################
-### code chunk number 2: vignette.Rnw:38-39 (eval = FALSE)
+### code chunk number 2: vignette.Rnw:41-42 (eval = FALSE)
 ###################################################
 ## system("hmmscan -h")
 
